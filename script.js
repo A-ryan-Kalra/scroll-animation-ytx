3e7950e99cb8cd426621f0b4cdfcab1f06bf4743
Aryan Kalra <aryan.smart832@gmail.com>
2024-12-28 14:56:38 +0530

    new practises applied

--- script.js
+++ script.js
@@ -9,9 +9,36 @@ function setScrollVar() {
     "--scroll",
     Math.min(percentOfHeightScrolled * 100, 100)
   );
-  console.log(htmlElement.style.getPropertyValue("--scroll"));
-  console.log(
-    getComputedStyle(document.querySelector(`img[alt="img-1"]`)).transform
-  );
+  htmlElement.style.setProperty("--scrollZ", percentOfHeightScrolled * 100);
+  // const img = document.querySelector('img[alt="deploy"]');
+  // console.log(img);
+  // img.style.transform = `rotate(${percentOfHeightScrolled * 100}deg)`;
+
+  // console.log(htmlElement.style.getPropertyValue("--scrollZ"));
+  //   console.log(
+  //     getComputedStyle(document.querySelector(`img[alt="img-1"]`)).transform
+  //   );
 }
 setScrollVar();
+
+// console.log(document.querySelector("#img3"));
+
+const observer = new IntersectionObserver((entries) => {
+  for (let i = entries.length - 1; i >= 0; i--) {
+    // console.log(entries[i]);
+    const entry = entries[i];
+    if (entry.isIntersecting) {
+      document
+        .querySelectorAll("[data-img]")
+        .forEach((img) => img.classList.remove("show"));
+      const img = document.querySelector(entry.target.dataset.imgToShow);
+      // console.log(img);
+      img.classList.add("show");
+      break;
+    }
+  }
+});
+
+document
+  .querySelectorAll("[data-img-to-show]")
+  .forEach((section) => observer.observe(section));
